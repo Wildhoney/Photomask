ad9cce04011e6bdbf5bbbb638af2231c175b9b0e
Wildhoney <adam.timberlake@gmail.com>
2015-11-18 10:11:20 +0000

    Refactored core with fundamentals

---
+++ src/core.js
@@ -0,0 +1,9 @@
+///**
+// * @method transform
+// * @param {HTMLElement} element
+// * @param {String} [image]
+// * @return {Promise}
+// */
+//export function transform(element, { image } = {}) {
+//
+//}
