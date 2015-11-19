b8b3dbb4d0f390c491a9147f2052ba45e8195e28
Wildhoney <adam.timberlake@gmail.com>
2015-11-19 17:46:24 +0000

    Added document.registerElement

--- src/core.js
+++ src/core.js
@@ -1,8 +1,44 @@
 /**
  * @method transform
  * @param {HTMLElement} element
+ * @param {String} [image]
+ * @param {String} [text]
  * @return {Promise}
  */
-export function transform(element) {
+export function transform(element, { image, text }) {
 
 }
+
+document.registerElement('x-photomask', {
+
+    /**
+     * @property extends
+     * @type {String}
+     */
+    extends: 'img',
+
+    /**
+     * @property prototype
+     * @type {Object}
+     * @return {void}
+     */
+    prototype: Object.create(HTMLImageElement.prototype, {
+
+    	/**
+    	 * @method attachedCallback
+    	 * @return {void}
+    	 */
+    	attachedCallback: () => {
+            transform(this);
+    	},
+
+        /**
+         * @method attributeChangedCallback
+         * @return {void}
+         */
+        attributeChangedCallback: () => {
+            transform(this);
+        }
+
+    })
+});
